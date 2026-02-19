e1ae04c8cc73d42e04cd9919a59c1de4ce571fb4
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-02-20 00:49:09 +0600

    added packages

--- package/sxiv.sh
+++ package/sxiv.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo pacman -S base-devel git imlib2 libx11 libxft libxinerama libxrandr
+
 path="$HOME/git"
 mkdir -p $path
 git clone https://github.com/HimadriChakra12/sxiv.git $path/sxiv
